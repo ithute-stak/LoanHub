7206d81f3889e82f3cf11e4b5080c7f6e1768dac
ithute-stak <thekoetlisi@tjekatjeka.co.ls>
2026-09-26 18:48:44 +0200

    Run credit committee regressions in release validation

--- scripts/validate_project.sh
+++ scripts/validate_project.sh
@@ -23,6 +23,7 @@ cd "$ROOT_DIR/apps/backend"
   tests/test_loan_folio_sequence.py \
   tests/test_complete_folio_book.py \
   tests/test_employer_payroll_management.py \
+  tests/test_credit_committee_underwriting.py \
   tests/test_assisted_employer_details_frontend.py \
   tests/test_contract_collection_cost_clause.py \
   tests/test_optional_contract_mandate.py \
